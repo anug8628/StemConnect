6753975d318658775103a7c2c6af23d07c0f43bb
Anushka <Anushka@Ravis-MacBook-Air.local>
2018-04-23 15:35:00 -0700

    Initial commit

---
+++ app/src/main/java/com/example/anushka/my_application/ChatMessage.java
@@ -0,0 +1,53 @@
+package com.example.anushka.my_application;
+
+        import android.support.annotation.NonNull;
+        import android.view.Menu;
+        import android.view.MenuItem;
+        import android.widget.Toast;
+        import java.util.Date;
+
+/**
+ * Created by Anushka on 4/19/18.
+ */
+public class ChatMessage {
+
+    private String messageText;
+    private String messageUser;
+    private long messageTime;
+
+    public ChatMessage(String messageText, String messageUser) {
+        this.messageText = messageText;
+        this.messageUser = messageUser;
+
+        // Initialize to current time
+        messageTime = new Date().getTime();
+    }
+
+    public ChatMessage(){
+
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
+
+    public String getMessageUser() {
+        return messageUser;
+    }
+
+    public void setMessageUser(String messageUser) {
+        this.messageUser = messageUser;
+    }
+
+    public long getMessageTime() {
+        return messageTime;
+    }
+
+    public void setMessageTime(long messageTime) {
+        this.messageTime = messageTime;
+    }
+}
